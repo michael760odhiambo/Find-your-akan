3ebd131c385f1dfd1b90c58455f5203c653e0a16
michael760odhiambo <michaelodhiambo760@gmail.com>
2019-07-14 16:56:43 +0300

    save this first js changes

--- script.js
+++ script.js
@@ -1,86 +1,53 @@
-var century, year, month, date, day;
-
-
-
-century = document.getElementById('century').value
-year = document.getElementById('year').value
-month = document.getElementById('month').value
-date= document.getElementById('date').value
-day = document.getElementById('day').value
-
-
-function cal(){
-
-  day = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date )% 7;
-
-
+//get input
+function getInput(){
+var century = parseInt(document.getElementById("century"))
+var year = parseInt(document.getElementById("year"))
+var month = parseInt(document.getElementById("month"))
+var date = parseInt(document.getElementById("date"))
 }
 
-function calradio(){
-  if(century < 19|| > 20){
-  alert("your value is invalid")}
+//calculate day of the week
+function calculate(){
+getInput();
+var day = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date )% 7;
+return(math.floor(day))
+}
+var maleNames = ["Kwasi", "Kwasi", "Kwebena", "Kwaku", "Yaw", "Kofi", "Kwame",]
+var femaleNames = ["Akosua", "Adwona", "Abenaa", "Akua", "Yaa", "Afua", "Ama",]
 
-}else if (year <= 89 || ) {
-  alert("enter a valid year")
-
-}else if (month <=1 && =>12) {
-  alert("your value is in valid")
-
-}else if (date < 1 >31) {
-  alert("your value is in valid")
-
+//radio button
+var rads = document.getElementByName("gender");{
+if(gender[0].checked == true){
+var gender = "male"
+}else if(gender[1].checked == true){
+var gender ="female"
+}else console.log("pass")
 }
 
-var radio = getElementsByName('gender'){
-  if (gender[0].checked === true) {
-    gender = "male"
-
-  }else if (gender[1].checked === true) {
-    gender = "female"
-
-  }else{
-    console.log("submit")
-  }
+//assining akan names and days of birth
+switch(gender){
+case "male":
+if(day == 0){document.write("sunday + maleNames")}
+else if(day == 1){document.write("monday + maleNames")}
+else if(day == 2){document.write("tuesnday + maleNames")}
+else if(day == 3){document.write("wednesday + maleNames")}
+else if(day == 4){document.write("thursday + maleNames")}
+else if(day == 5){document.write("friaday + maleNames")}
+else if(day == 6){document.write("saturday + maleNames")
 }
 
-var male = "true";
-if (day===0) {
-  console.log("you were born on sunday and your name is kwasi")
-} else if (day===1) {
-  console.log("you were born on monday and your name is kwadwo")
-}if (day===2) {
-  console.log("you were born on tuesday and your name is kwabena")
-}if (day===3) {
-  console.log("you were born on wednesday and your name is kwaku")
-}if (day===4) {
-  console.log("you were born on thursday and your name is yaw")
-}if (day===5) {
-  console.log("you were born on friaday and your name is kofi")
-}if (day===6) {
-  console.log("you were born on saturday and your name is kwame")
-}
 
-}
+case "female":
+if(day == 0){document.write("sunday + femaleNames")}
+else if(day == 1){document.write("monday + femaleNames")}
+else if(day == 2){document.write("tuesnday + femaleNames")}
+else if(day == 3){document.write("wednesday + femaleNames")}
+else if(day == 4){document.write("thursday + femaleNames")}
+else if(day == 5){document.write("friaday + femaleNames")}
+else if(day == 6){document.write("saturday + femaleNames")}
 
-}
-var female = "true";
-if (day===0) {
-  console.log(alert("you were born on sunday and your name is Akosua"))
 
-} else if (day===1) {
-  console.log("you were born on monday and your name is Adwoa")
-}if (day===2) {
-  console.log("you were born on tuesday and your name is Abenaa")
-}if (day===3) {
-  console.log("you were born on wednesday and your name is Akua")
-}if (day===4) {
-  console.log("you were born on thursday and your name is Yaa")
-}if (day===5) {
-  console.log("you were born on friaday and your name is Afua")
-}if (day===6) {
-  console.log("you were born on saturday and your name is Ama")
-}
-
-}
-
-}
+//break
+default
+console.log(getInput)
+//call all function in html btn
